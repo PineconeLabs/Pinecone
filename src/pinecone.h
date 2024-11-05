39d07f21826b38a20225012ac080f93853277e05
CI <-ci@not-real.com>
2024-11-05 21:03:03 +0100

    added matrixCreate and matrixPrint

--- src/pinecone.h
+++ src/pinecone.h
@@ -3,17 +3,24 @@ Pinecone 0.0.1 - A simple and easy-to-use library for Machine learning
 */
 
 
+// Includes
+#include <stddef.h>
 
 
+
+
+// Definitions
+
+#define MAT_AT(m, i, j) m.es[(i)*(m).cols + (j)]
 
 
 // Types and structs
 
 typedef struct Matrix
 {
-   int rows;
-   int cols;
-   int stride;
+   size_t rows;
+   size_t cols;
+   size_t stride;
    float *es; 
 } Matrix;
 
@@ -24,4 +31,8 @@ typedef struct Matrix
 // Function Declarations
 
 
-Matrix matrix_alloc(int rows, int cols);
+Matrix matrixCreate(size_t rows, size_t cols);
+Matrix matrixDot(Matrix m1, Matrix m2);
+Matrix matrixSum(Matrix m1, Matrix m2);
+
+void matrixPrint(Matrix matrix);
